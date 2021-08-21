54925b89346585a2c0129fecc905ca34e0262d1d
Stas <staszhingel@icloud.com>
2021-08-21 19:11:15 +0300

    UICollectioViewCompositionLayout

---
+++ CollectionViewRayLessons/NumberCell.swift
@@ -0,0 +1,14 @@
+//
+//  NumberCell.swift
+//  CollectionViewRayLessons
+//
+//  Created by Стас Жингель on 21.08.2021.
+//
+
+import UIKit
+
+class NumberCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+    static let reuseIdentifier = String(describing: NumberCell.self)
+    
+}
--- CollectionViewRayLessons/ViewController.swift
+++ CollectionViewRayLessons/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    enum Section{
+        case main
+        
+    }
+    @IBOutlet weak var collectionView: UICollectionView!
+    var dataSource : UICollectionViewDiffableDataSource<Section, Int>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.collectionViewLayout = configureLayout()
+        configureDataSource()
     }
 
-
+    func configureLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension:  .fractionalWidth(0.333333), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2 , leading: 2, bottom: 2, trailing: 2 )
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.33333333))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: self.collectionView) {
+         (collectionView, indexPath, number) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NumberCell.reuseIdentifier, for: indexPath) as? NumberCell else { fatalError("cannot create new cell ")}
+            cell.label.text = number.description
+            cell.backgroundColor = .yellow
+            return cell
+        }
+        var initialSnapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        initialSnapshot.appendSections([.main])
+        initialSnapshot.appendItems(Array(1...100))
+        
+        dataSource.apply(initialSnapshot, animatingDifferences: false)
+      
+    }
 }
 
